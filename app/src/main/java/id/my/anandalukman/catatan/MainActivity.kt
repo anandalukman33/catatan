f8c72e609478252130b441f4ebd8ba4cc6c56b81
Ananda Muhamad Lukman <ananda.muhamad.lukman@gmail.com>
2021-11-01 01:19:55 +0700

    Initial commit

---
+++ app/src/main/java/id/my/anandalukman/catatan/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.my.anandalukman.catatan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
